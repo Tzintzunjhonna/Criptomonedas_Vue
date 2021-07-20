86fda29010de756abdb1a6a84a601fe5d141235e
Tzintzunjhonna <jjctjhonathan@gmail.com>
2021-07-20 12:41:22 -0500

    Renderizando Listas

--- Vue/app.js
+++ Vue/app.js
@@ -6,6 +6,17 @@ new Vue ({
       name: 'Bitcoin',
       img :'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: -1,
+      prices: [8400, 5640, 6956, 6984, 659465, 5461],
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+    ],
+    
     }
   }
 })
