1283cc4f385aed18c31d6fba8fadef9770865ddf
Tzintzunjhonna <jjctjhonathan@gmail.com>
2021-07-20 15:06:20 -0500

    Computer and watches

--- Vue/app.js
+++ Vue/app.js
@@ -4,8 +4,9 @@ new Vue ({
   data () {
     return {
       name: 'Bitcoin',
+      symbol: 'BTC',
       img :'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 1,
+      changePercent: -10,
       color: '#000000',
       prices: 8400,
       pricesWithDays: [
@@ -18,6 +19,18 @@ new Vue ({
         { day: 'Domingo', value: 10200 },
     ],
       showPrices: false
+    }
+  },
+
+  computed:{
+    title () {
+      return `${this.name} - ${this.symbol}`
+    }
+  },
+
+  watch: {
+    showPrices (newVal, oldVal) {
+      console.log(newVal, oldVal)
     }
   },
 
