5d3289c9452666fb527408cfa217e1ae8fab178d
Tzintzunjhonna <jjctjhonathan@gmail.com>
2021-07-20 19:58:02 -0500

    Comunicación entre Componentes: propiedades

--- Vue/app.js
+++ Vue/app.js
@@ -1,25 +1,23 @@
-new Vue ({
-  el: '#app',
+//Crear un nuevo componente 
+
+Vue.component('CoinDetail', {
+
+// Pasar las propiedades a html con las propiedades correspondientes
+  props: ['coin'],
 
   data () {
     return {
-      name: 'Bitcoin',
-      symbol: 'BTC',
-      img :'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: -10,
-      value : 0,
-      color: '#000000',
-      prices: 8400,
-      pricesWithDays: [
-        { day: 'Lunes', value: 8400 },
-        { day: 'Martes', value: 7900 },
-        { day: 'Miercoles', value: 8200 },
-        { day: 'Jueves', value: 9000 },
-        { day: 'Viernes', value: 9400 },
-        { day: 'Sabado', value: 10000 },
-        { day: 'Domingo', value: 10200 },
-    ],
-      showPrices: false
+      showPrices: false,
+      value:0,
+
+      
+    }
+  },
+
+  methods: {
+
+    toggleShowPrices () {
+      this.showPrices = !this.showPrices
     }
   },
 
@@ -27,7 +25,7 @@ new Vue ({
 
     //Mostrar el nombre del Bitcoin
     title () {
-      return `${this.name} - ${this.symbol}`
+      return `${this.coin.name} - ${this.coin.symbol}`
     },
 
     // Convertir el valor ingresado a el valor de bitcoin
@@ -36,24 +34,95 @@ new Vue ({
         return 0
       }
 
-      return this.value / this.prices
+      return this.value / this.coin.prices
     }
   },
 
-  // trackear o rastrear propiedades en tiempo real, movimientos que se hacen
-  watch: {
-    showPrices (newVal, oldVal) {
-      console.log(newVal, oldVal)
+  template:
+  
+  `
+  <div>
+      <img
+        v-on:mouseover="toggleShowPrices"
+        v-on:mouseout="toggleShowPrices"
+        v-bind:src="coin.img"
+        alt="coin.name"
+      />
+
+    <!-- Mostrar titulo con computed -->
+
+      <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
+        {{ title }}
+        <span v-if="coin.changePercent > 0">👍</span>
+        <span v-else-if="coin.changePercent < 0">👎</span>
+        <span v-else>👩‍🦱</span>
+
+        <!-- boton mostrar precios con cambio de emoji -->
+
+        <span v-on:click="toggleShowPrices">
+          Ver precios{{ showPrices ? '👼' : '👮‍♀️' }}
+        </span>
+      </h1>
+
+    <input type="number" v-model="value" />
+    <span> {{ convertedValue }} </span>
+
+    <ul v-show="showPrices">
+        <!-- Mostrar precios de un color dependiendo los precios -->
+
+        <li
+          class="uppercase"
+          v-bind:class="{ orange: p.value == coin.prices, red: p.value < coin.prices, green: p.value > coin.prices }"
+          v-for="(p, i) in coin.pricesWithDays"
+          v-bind:key="p.day"
+        >
+          {{ i }} - {{ p.day }} - {{ p.value }}
+        </li>
+      </ul>
+
+  </div>
+
+  `
+})
+
+new Vue ({
+  el: '#app',
+
+  data () {
+    return {
+      btc:{
+        name: 'Bitcoin',
+        symbol: 'BTC',
+        img :'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+        changePercent: -10,
+        prices: 8400,
+        pricesWithDays: [
+          { day: 'Lunes', value: 8400 },
+          { day: 'Martes', value: 7900 },
+          { day: 'Miercoles', value: 8200 },
+          { day: 'Jueves', value: 9000 },
+          { day: 'Viernes', value: 9400 },
+          { day: 'Sabado', value: 10000 },
+          { day: 'Domingo', value: 10200 },
+      ],
+    },
+      
+      color: '#000000',
+      
     }
   },
 
-  methods:{
-    //Mostrar lista de los precios
-    toggleShowPrices () {
-      this.showPrices = !this.showPrices
 
-      // Convertir el color de background a su formato reverso
-      this.color = this.color.split('').reverse().join('')
-    }
-  }
+
+
+
+    // methods:{
+    //   //Mostrar lista de los precios
+    //   toggleShowPrices () {
+    //     this.showPrices = !this.showPrices
+
+    //     // Convertir el color de background a su formato reverso
+    //     this.color = this.color.split('').reverse().join('')
+    //   }
+    // }
 })
